bd4022ef643d0ed4e85e626e881e9d6892f16d52
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-24 04:56:51 +0530

    Update Intro.jsx

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -12,8 +12,8 @@ const Intro = () => {
         <div className="  px-4  py-2 flex justify-center flex-col  items-center gap-6 sm:gap-2 mt-4 ">
         
           <div className=" max-w-6xl">
-          <h2 className="font-groot-one text-3xl my-2 text-[#000000] text-center font-bold relative z-30 -mt-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam illo alias voluptatibus eos veritatis? Laboriosam tempore repudianda
+          <h2 className="font-groot-one text-2xl my-2 text-[#000000] text-center font-bold relative z-30 -mt-4">
+          PugWIF is the ultimate meme coin on Solana, led by our charismatic PugWIF, the true dog leader of the meme community. With irresistible charm and honest intentions, hearts are won and investors wooed left, right, and center.
         </h2>
           
            <div className=" grid grid-cols-1 sm:grid-cols-2 items-center gap-4 mt-12">
